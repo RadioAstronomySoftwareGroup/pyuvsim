bd4d5cb2c66d9f8743ae69ed7a4dbc7f3c80509f
Adam Lanman <adam_lanman@brown.edu>
2018-05-24 16:59:40 -0700

    Ignore warnings when rank != 0

--- scripts/run_pyuvsim.py
+++ scripts/run_pyuvsim.py
@@ -8,13 +8,16 @@ from mpi4py import MPI
 from pyuvdata import UVBeam, UVData
 from pyuvdata.data import DATA_PATH
 from pyuvsim.data import DATA_PATH as SIM_DATA_PATH
+import warnings
 
-
-beam_file = os.path.join(DATA_PATH, 'HERA_NicCST_150MHz.txt')
+beam_files = os.path.join(DATA_PATH, 'HERA_NicCST_150MHz.txt')
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+
+if not rank == 0:
+    warnings.simplefilter('ignore')
 
 parser = argparse.ArgumentParser(description=("A command-line script "
                                               "to execute a pyuvsim simulation."))
@@ -29,11 +32,14 @@ parser.add_argument('--mock_arrangement', type=str,default='zenith')
 args = parser.parse_args()
 
 for filename in args.file_in:
-    print("Reading:", os.path.basename(filename))
+    if rank == 0:
+        print("Reading:", os.path.basename(filename))
     input_uv = UVData()
-    input_uv.read_uvfits(filename)
+    input_uv.read_uvfits(filename,read_data=False)
+    time0 = input_uv.time_array[0]
+    input_uv.read_uvfits(filename, freq_chans=0, times=time0)  # "Doesn't need to be a list" -- Matt
     beam = UVBeam()
-    beam.read_cst_beam(beam_file, beam_type='efield', frequency=150e6,
+    beam.read_cst_beam(beam_files, beam_type='efield', frequency=[150e6],
                        telescope_name='HERA',
                        feed_name='PAPER', feed_version='0.1', feed_pol=['x'],
                        model_name='E-field pattern - Rigging height 4.9m',
