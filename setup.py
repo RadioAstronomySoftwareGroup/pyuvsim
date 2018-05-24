78e49f2a0c34c0c2253324c06b849ec339b16edd
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-05-24 13:50:11 -0700

    pep8 fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import json
 
 # data = [version.git_origin, version.git_hash, version.git_description, version.git_branch]
 # with open(op.join('pyuvsim', 'GIT_INFO'), 'w') as outfile:
-    # json.dump(data, outfile)
+#     json.dump(data, outfile)
 
 setup_args = {
     'name': 'pyuvsim',
@@ -19,7 +19,7 @@ setup_args = {
     'scripts': glob.glob('scripts/*'),
     'version': '0.0.1',
     'include_package_data': True,
-    'install_requires': ['numpy>=1.10', 'astropy>=1.2',  'pyuvdata'],
+    'install_requires': ['numpy>=1.10', 'astropy>=1.2', 'pyuvdata'],
     'classifiers': ['Development Status :: 2 - Pre-Alpha',
                     'Intended Audience :: Science/Research',
                     'License :: OSI Approved :: BSD License',
