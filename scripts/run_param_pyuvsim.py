67a3f8fbbe553fc2fabebb87a5ae74b20f99b60a
Adam Lanman <adam_lanman@brown.edu>
2018-05-25 19:37:04 -0700

    Update to run_param_pyuvsim.py

--- scripts/run_param_pyuvsim.py
+++ scripts/run_param_pyuvsim.py
@@ -8,48 +8,8 @@ from mpi4py import MPI
 from pyuvdata import UVBeam, UVData
 from pyuvdata.data import DATA_PATH
 from pyuvsim.data import DATA_PATH as SIM_DATA_PATH
+from pyuvsim import simsetup
 
-
-def fill_defaults(p):
-    """
-        If value is unset in the parameters dictionary p, set it to a default value.
-        
-    """
-    assert( isinstance(p,dict) )
-    sim_required_params = {'catalog' : 'mock', 'outdir' : '.', 'outfile_prefix' : 'sim', 'beam_file' : os.path.join(DATA_PATH, 'HERA_NicCST_150MHz.txt')}
-    req_params_uvfile = { 'uvfile': os.path.join(SIM_DATA_PATH,'28mEWbl_1time_1chan.uvfits')}    #Required params for simulating off a uvfile
-    req_params_nofile = { 'start_time' : None,
-                            'end_time' : None,
-                            'integration_time' : 1.0,
-                            'Ntimes' : None,
-                            'start_freq': None,
-                            'end_freq' : None,
-                            'Nfreqs' : None,
-                            'channel_width': None,
-                        }
-    
-    if  'uvfile' in p.keys() or len(p.keys()) == 0:
-        sim_required_params.update(req_params_uvfile)
-    else:
-        sim_required_params.update(req_params_nofile)
-    
-    # Loop over the req_params_use and fill values in p that are missing.
-
-    for k in sim_required_params.keys():
-        if not k in p.keys():
-            print("Missing parameter "+str(k)+", defaulting to "+str(sim_required_params[k]))
-            p[k] = sim_required_params[k]
-
-    # Certain parameters cannot be missing and have no default. Error if they're None.
-    for k in p.keys():
-        if p[k] is None: raise ValueError("Parameter "+str(k)+" must be defined.")
-
-    if (p['catalog'] == 'mock') and ('mock_arrangement' not in p):
-        print("Using mock catalog and arrangement unset. Defaulting to \'zenith\'")
-        p['mock_arrangement'] = 'zenith'
-        p['Nsrcs'] = 1
-
-    return p
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -70,25 +30,57 @@ with open(args['paramsfile'], 'r') as pfile:
 if params is None:
     params = {}
 
-print params
-params = fill_defaults(params)
+if rank == 0:
 
-if 'uvfile' in params:
-    ### simulate from a uvfits file
-    filename = params['uvfile']
-    print("Reading:", os.path.basename(filename))
-    input_uv = UVData()
-    input_uv.read_uvfits(filename)
-    beam = UVBeam()
-    beam.read_cst_beam(params['beam_file'], beam_type='efield', frequency=150e6,
-                       telescope_name='HERA',
-                       feed_name='PAPER', feed_version='0.1', feed_pol=['x'],
-                       model_name='E-field pattern - Rigging height 4.9m',
-                       model_version='1.0')
+    if 'uvfile' in params:
+        ### simulate from a uvfits file if one is specified in the param file.
+    
+        filename = params['uvfile']
+        print("Reading:", os.path.basename(filename))
+        input_uv = UVData()
+        input_uv.read_uvfits(filename)
+        beam_ids = params['beam_files'].keys()
+        beamfits_files = params['beam_files'].values()
+        beam_list = []
+        for bf in beamfits_files:
+            uvb = UVBeam()
+            uvb.read_beamfits()
+            beam_list.append(bf)
+    
+        beam_list = (np.array(beam_list)[beam_ids]).tolist()
+        outfile_name = os.path.join(params['outdir'], params['outfile_prefix'] + "_" + os.path.basename(filename))
+    
+    else:
+        ### Not running off a uvfits.
+    
+        input_uv, beam_list, beam_ids = simsetup.initialize_uvdata_from_params(params)
 
-    beam_list = [beam]
-    uvdata_out = pyuvsim.uvsim.run_uvsim(input_uv, beam_list=beam_list, mock_arrangement=params['mock_arrangement'], Nsrcs=params['Nsrcs'])
-    if rank == 0:
-        outfile = os.path.join(params['outdir'], params['outfile_prefix'] + "_" + os.path.basename(filename))
-        if not os.path.exists(params['outdir']): os.makedirs(params['outdir'])
-        uvdata_out.write_uvfits(outfile, force_phase=True, spoof_nonessential=True)
+        file_params = params['filing']
+        params.update(file_params)
+        del params['filing']
+        if not 'outfile_name' in params or params['outfile_name'] == '':
+            outfile_prefix = ""
+            outfile_suffix = "_results"
+            if 'outfile_prefix' in params:
+                outfile_prefix = params['outfile_prefix']+"_"
+            if 'outfile_suffix' in params:
+                outfile_suffix = "_"+params['outfile_suffix']
+            outfile_name = os.path.join(params['outdir'], outfile_prefix +os.path.basename(args['paramsfile'])[:-5] + outfile_suffix)  #Strip .yaml extention
+        else:
+            outfile_name = params['outfile_name']
+
+        if not 'clobber' in params:
+            outfile_name = simsetup.check_file_exists_and_increment(outfile_name)
+
+        source_params = params['sources']
+        if source_params['catalog'] == 'mock':
+            params['mock_arrangement'] = source_params['mock_arrangement']   ## TODO Would like to pass kwargs to create_mock_catalog
+
+
+uvdata_out = pyuvsim.uvsim.run_uvsim(input_uv, beam_list=beam_list, mock_arrangement=params['mock_arrangement'])
+
+if rank ==0:
+    if not os.path.exists(params['outdir']): os.makedirs(params['outdir'])
+    uvdata_out.write_uvfits(outfile_name, force_phase=True, spoof_nonessential=True)
+
+
