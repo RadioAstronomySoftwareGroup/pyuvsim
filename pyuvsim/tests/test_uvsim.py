81aa20fdd60f1f1859914d2046fde509340f5d3d
Adam Lanman <adam_lanman@brown.edu>
2018-07-23 14:23:53 -0400

    Remove serial_uvsim function

--- pyuvsim/tests/test_uvsim.py
+++ pyuvsim/tests/test_uvsim.py
@@ -796,27 +796,6 @@ def test_gather():
     nt.assert_true(np.allclose(uv_out.data_array, hera_uv.data_array, atol=5e-3))
 
 
-def test_run_serial_uvsim():
-    hera_uv = UVData()
-    hera_uv.read_uvfits(EW_uvfits_file)
-
-    beam = UVBeam()
-    beam.read_cst_beam(beam_files, beam_type='efield', frequency=[100e6, 123e6],
-                       telescope_name='HERA',
-                       feed_name='PAPER', feed_version='0.1', feed_pol=['x'],
-                       model_name='E-field pattern - Rigging height 4.9m',
-                       model_version='1.0')
-    beam_list = [beam]
-
-    uv_out = pyuvsim.run_serial_uvsim(hera_uv, beam_list, catalog=None, Nsrcs=1,
-                                      uvdata_file=EW_uvfits_file)
-
-    print np.round(uv_out.data_array)
-    print hera_uv.data_array
-
-    nt.assert_true(np.allclose(uv_out.data_array, hera_uv.data_array, atol=5e-3))
-
-
 def test_sources_equal():
     time = Time('2018-03-01 00:00:00', scale='utc')
     src1 = create_zenith_source(time, 'src')
