9ebcf8ff530aa3b0f172aad1c9b3ac8c072bacbb
Adam Lanman <adam_lanman@brown.edu>
2018-08-25 00:19:42 -0400

    Test for cirs/tee transformations

--- pyuvsim/tests/test_uvsim.py
+++ pyuvsim/tests/test_uvsim.py
@@ -921,3 +921,11 @@ def test_gaussbeam_values():
 
     beam_values = np.exp(-(zenith_angles)**2 / (2 * beam.sigma**2))
     nt.assert_true(np.all(beam_values**2 == coherencies))
+
+
+def test_tee_ra_loop():
+    time = Time(2457458.1739, scale='utc', format='jd')
+    tee_ra = Angle(np.pi/4., unit='rad')  #rad
+    cirs_ra = pyuvsim.utils.tee_to_cirs_ra(tee_ra, time)
+    new_tee_ra = pyuvsim.utils.cirs_to_tee_ra(cirs_ra, time)
+    nt.assert_equal(new_tee_ra, tee_ra)
