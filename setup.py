72fdee4fde38acc05f23bc20c787779c3d47c7cc
Bryna Hazelton <bryna.hazelton@gmail.com>
2018-09-05 10:53:05 -0700

    Add six as a required package

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup_args = {
     'scripts': glob.glob('scripts/*'),
     'version': version.version,
     'include_package_data': True,
-    'install_requires': ['numpy>=1.10', 'astropy>=1.2', 'pyuvdata'],
+    'install_requires': ['numpy>=1.10', 'astropy>=1.2', 'six>=1.10', 'pyuvdata'],
     'classifiers': ['Development Status :: 2 - Pre-Alpha',
                     'Intended Audience :: Science/Research',
                     'License :: OSI Approved :: BSD License',
