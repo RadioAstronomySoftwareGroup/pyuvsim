fd09c6f708ed3186095491bfdb293c0102c493d0
aelanman <adam_lanman@brown.edu>
2018-09-20 15:55:19 -0400

    Fixes to make wrapper work with new mpi structure

--- scripts/run_param_pyuvsim.py
+++ scripts/run_param_pyuvsim.py
@@ -5,24 +5,17 @@
 
 from __future__ import absolute_import, division, print_function
 
+import pyuvsim
 import argparse
 import os
-import yaml
 import numpy as np
-from astropy.coordinates import EarthLocation
-from mpi4py import MPI
-
+import yaml
 from pyuvdata import UVBeam, UVData
 from pyuvdata.data import DATA_PATH
 from pyuvsim.data import DATA_PATH as SIM_DATA_PATH
-
-import pyuvsim
 from pyuvsim import simsetup
-
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+from astropy.coordinates import EarthLocation
+from pyuvsim import mpi
 
 parser = argparse.ArgumentParser(description=("A command-line script "
                                               "to execute a pyuvsim simulation from a parameter file."))
@@ -36,16 +29,23 @@ if 'paramsfile' not in args:
 with open(args['paramsfile'], 'r') as pfile:
     params = yaml.safe_load(pfile)
 
+params['config_path'] = '.'
+
 if params is None:
     params = {}
 
 input_uv = UVData()
 
+Nbeams = 0
 beam_list = None
 beam_dict = None
 input_uv = UVData()
 mock_keywords = None
 catalog = None
+
+mpi.start_mpi()
+rank = mpi.get_rank()
+
 if rank == 0:
     if 'uvfile' in params:
         # simulate from a uvfits file if one is specified in the param file.
@@ -59,18 +59,18 @@ if rank == 0:
             beamfits_files = params['beam_files'].values()
             beam_list = []
             for bf in beamfits_files:
-                uvb = UVBeam()
-                uvb.read_beamfits()
                 beam_list.append(bf)
 
         beam_list = (np.array(beam_list)[beam_ids]).tolist()
+        Nbeams = len(beam_list)
         outfile_name = os.path.join(params['outdir'], params['outfile_prefix'] + "_" + os.path.basename(filename))
         outfile_name = outfile_name + ".uvfits"
 
     else:
         # Not running off a uvfits.
         print("Simulating from parameters")
-        input_uv, beam_list, beam_dict, beam_ids = simsetup.initialize_uvdata_from_params(args['paramsfile'])
+        input_uv, beam_list, beam_dict, beam_ids = simsetup.initialize_uvdata_from_params(params)
+        Nbeams = len(beam_list)
         print("Nfreqs: ", input_uv.Nfreqs)
         print("Ntimes: ", input_uv.Ntimes)
         source_params = params['sources']
@@ -87,7 +87,10 @@ if rank == 0:
             catalog = source_params['catalog']
         else:
             catalog = None
+# mpi.start_mpi()
+#beam_dict = mpi.get_comm().bcast(beam_dict, root=0)
 uvdata_out = pyuvsim.uvsim.run_uvsim(input_uv, beam_list=beam_list, beam_dict=beam_dict, catalog_file=catalog, mock_keywords=mock_keywords)
+
 
 if rank == 0:
     simsetup.write_uvfits(uvdata_out, params)
