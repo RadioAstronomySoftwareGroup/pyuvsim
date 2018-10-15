Broadcast beam strings instead of objects and fix profiling wrapper

pyuvsim/simsetup.py
@@ -349,35 +349,25 @@ def initialize_uvdata_from_params(obs_params):
     beam_ids = ant_layout['beamid']
     beam_list = []
     beam_dict = {}
+
     for beamID in np.unique(beam_ids):
         beam_model = telparam['beam_paths'][beamID]
         which_ants = antnames[np.where(beam_ids == beamID)]
         for a in which_ants:
             beam_dict[a] = beamID
-        uvb = UVBeam()
-        if beam_model in ['gaussian', 'uniform', 'airy']:
-            # Identify analytic beams
-            if beam_model == 'gaussian':
-                if 'sigma' not in telparam:
-                    raise KeyError("Missing sigma for gaussian beam.")
-                beam = AnalyticBeam('gaussian', sigma=telparam['sigma'])
-            elif beam_model == 'airy':
-                if 'diameter' not in telparam:
-                    raise KeyError("Missing diameter for airy beam")
-                beam = AnalyticBeam('airy', diameter=telparam['diameter'])
-            else:
-                beam = AnalyticBeam('uniform')
-            beam_list.append(beam)
-            continue
-        if not os.path.exists(beam_model):
-            filename = beam_model
-            path = os.path.join(SIM_DATA_PATH, filename)
-            if not os.path.exists(path):
-                raise OSError("Could not find beam file " + filename)
-        else:
-            path = beam_model   # beam_model = path to beamfits
-        uvb.read_beamfits(path)
-        beam_list.append(uvb)
+        if beam_model == 'gaussian':
+            try:
+                sigma = telparam['sigma']
+                beam_model = beam_model + '_' + str(sigma)
+            except KeyError:
+                raise KeyError("Missing sigma for gaussian beam.")
+        if beam_model == 'airy':
+            try:
+                diam = telparam['diameter']
+                beam_model = beam_model + '_' + str(diam)
+            except KeyError:
+                raise KeyError("Missing diameter for airy beam.")
+        beam_list.append(beam_model)
 
     param_dict['Nants_data'] = antnames.size
     param_dict['Nants_telescope'] = antnames.size
@@ -672,6 +662,31 @@ def uvdata_to_config_file(uvdata_in, param_filename=None, telescope_config_name=
 
     with open(os.path.join(path_out, param_filename), 'w') as yfile:
         yaml.dump(param_dict, yfile, default_flow_style=False)
+
+
+def beam_string_to_object(beam_model):
+    """
+        Make a beam object given an identifying string.
+    """
+    # Identify analytic beams
+    if beam_model.startswith('gaussian'):
+        sigma = float(beam_model.split("_")[1])
+        return AnalyticBeam('gaussian', sigma=sigma)
+    elif beam_model.startswith('airy'):
+        diameter = float(beam_model.split("_")[1])
+        return AnalyticBeam('airy', sigma=diameter)
+    elif beam_model.startswith('uniform'):
+        return AnalyticBeam('uniform')
+    if not os.path.exists(beam_model):
+        filename = beam_model
+        path = os.path.join(SIM_DATA_PATH, filename)
+        if not os.path.exists(path):
+            raise OSError("Could not find file " + filename)
+    else:
+        path = beam_model   # beam_model = path to beamfits
+    uvb = UVBeam()
+    uvb.read_beamfits(path)
+    return uvb
 
 
 def write_uvfits(uv_obj, param_dict, return_filename=False, dryrun=False):

pyuvsim/uvsim.py
@@ -324,7 +324,9 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
 
     Arguments:
         input_uv: An input UVData object, containing baseline/time/frequency information.
-        beam_list: A list of UVBeam and/or AnalyticBeam objects
+        beam_list: A list of UVBeam and/or AnalyticBeam identifier strings.
+
+    Keywords:
         beam_dict: Dictionary of {antenna_name : beam_ID}, where beam_id is an index in
                    the beam_list. This assigns beams to antennas.
                    Default: All antennas get the 0th beam in the beam_list.
@@ -343,7 +345,7 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
         raise TypeError("input_uv must be UVData object")
     # The Head node will initialize our simulation
     # Read input file and make uvtask list
-    catalog
+    catalog=None
     if rank == 0:
         print('Nblts:', input_uv.Nblts)
         print('Nfreqs:', input_uv.Nfreqs)
@@ -400,6 +402,9 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
 
     catalog = comm.bcast(catalog, root=0)
     input_uv = comm.bcast(input_uv, root=0)
+    beam_list = comm.bcast(beam_list, root=0)
+    beam_dict = comm.bcast(beam_dict, root=0)
+
     Nblts = input_uv.Nblts
     Nbls = input_uv.Nbls
     Ntimes = input_uv.Ntimes
@@ -407,7 +412,6 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
     Nsrcs = len(catalog)
 
     Ntasks = Nblts * Nfreqs * Nsrcs
-    catalog = comm.bcast(catalog, root=0)
 
     stride = Ntasks // Npus
     if (rank + 1) * stride >= Ntasks:
@@ -415,7 +419,10 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
     else:
         task_ids = np.arange(rank * stride, (rank + 1) * stride)
 
-    local_task_iter = uvdata_to_task_iter(task_ids, input_uv, catalog, beam_list, beam_dict)
+    # Construct beam objects from strings 
+    beam_models = [ simsetup.beam_string_to_object(bm) for bm in beam_list ]
+
+    local_task_iter = uvdata_to_task_iter(task_ids, input_uv, catalog, beam_models, beam_dict)
 
     summed_task_dict = {}
 
@@ -454,7 +461,7 @@ def run_uvsim(input_uv, beam_list, beam_dict=None, catalog_file=None,
         del task.source
         del task.baseline
         del task.telescope
-        
+
     # gather all the finished local tasks into a list of list of len NPUs
     # gather is a blocking communication, have to wait for all PUs
     full_tasklist = comm.gather(summed_local_task_list, root=0)

scripts/run_profile_pyuvsim.py
@@ -47,7 +47,6 @@ beam_dict = None
 input_uv = UVData()
 mock_keywords = None
 catalog = 'mock'
-print(catalog)
 if rank == 0:
 
     params['freq']['Nfreqs'] = args.Nfreqs
@@ -57,8 +56,15 @@ if rank == 0:
 
     # Baseline selection:
     input_uv.baseline_array = np.repeat(input_uv.baseline_array[:args.Nbls], args.Ntimes)
-    input_uv.ant_1_array = np.repeat(input_uv.ant_1_array[:args.Nbls], args.Ntimes)
-    input_uv.ant_2_array = np.repeat(input_uv.ant_2_array[:args.Nbls], args.Ntimes)
+    input_uv.ant_1_array, input_uv.ant_2_array = input_uv.baseline_to_antnums(input_uv.baseline_array)
+    ants_new = np.unique(input_uv.ant_1_array.tolist() + input_uv.ant_2_array.tolist())
+    input_uv.antenna_numbers = ants_new
+    input_uv.antenna_names = ants_new.astype(str)  # Antnames/numbers are going to be messed up by the baseline selection. Unimportant.
+    Nants = ants_new.size
+    beam_dict = dict(zip(input_uv.antenna_names, np.zeros(Nants, dtype=int)))  # For now, all use the same beam model
+    input_uv.antenna_positions = input_uv.antenna_positions[:Nants,:]
+    input_uv.Nants_data = Nants
+    input_uv.Nants_telescope = Nants
 
     # Time selection:
     inds = np.array([np.arange(args.Nbls) + i * input_uv.Nbls for i in range(args.Ntimes)]).flatten()