8203a1966470ed5fb256436096ef536909bc55e9
Adam Lanman <adam_lanman@brown.edu>
2018-10-31 17:55:43 -0400

    Python3 generators lack a next() function

--- pyuvsim/tests/test_uvsim.py
+++ pyuvsim/tests/test_uvsim.py
@@ -618,9 +618,9 @@ def test_local_task_gen():
 
     # Check error conditions
     uv_iter0 = pyuvsim.uvdata_to_task_iter(np.arange(Ntasks), 'not_uvdata', sources, beam_list, beam_dict)
-    nt.assert_raises(TypeError, uv_iter0.next)
+    nt.assert_raises(TypeError, next, uv_iter0)
     uv_iter1 = pyuvsim.uvdata_to_task_iter(np.arange(Ntasks), hera_uv, 'not_ndarray', beam_list, beam_dict)
-    nt.assert_raises(TypeError, uv_iter1.next)
+    nt.assert_raises(TypeError, next, uv_iter1)
 
     engine0 = pyuvsim.UVEngine(reuse_spline=False)
     for tki, task0 in enumerate(uvtask_iter):
