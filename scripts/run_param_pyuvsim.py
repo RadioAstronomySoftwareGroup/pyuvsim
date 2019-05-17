46d84b2712163480baefe9df3148a2922b2642c2
aelanman <adam_lanman@brown.edu>
2019-05-17 01:32:39 -0400

    Enable default time profiling in wrapper script

--- scripts/run_param_pyuvsim.py
+++ scripts/run_param_pyuvsim.py
@@ -14,10 +14,15 @@ parser = argparse.ArgumentParser(description=("A command-line script "
                                               "to execute a pyuvsim simulation from a parameter file."))
 
 parser.add_argument('paramsfile', type=str, help='Parameter yaml file.', default=None)
+parser.add_argument('--profile', type=str, help='Time profiling output file', default=None)
+
 args = parser.parse_args()
 
 if args.paramsfile is None:
     raise ValueError("Parameter file required")
+
+if args.profile is not None:
+    pyuvsim.profiling.set_profiler(outfile_name=args.profile)
 
 if not os.path.isdir(os.path.dirname(args.paramsfile)):
     args.paramsfile = os.path.join('.', args.paramsfile)
