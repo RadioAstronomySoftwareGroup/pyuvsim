e6e9d92c2320f113b8fc06b59b5c05858bf32434
dseitova <daniya.seitova@gmail.com>
2019-07-02 13:30:06 -0400

    added conversion Jy to Kstr function to utils.py and updated healpix function

--- pyuvsim/utils.py
+++ pyuvsim/utils.py
@@ -18,6 +18,7 @@ from astropy import _erfa as erfa
 from astropy.coordinates import Angle
 from astropy.coordinates.builtin_frames.utils import get_jd12
 from astropy.time import Time
+from astropy.constants import c
 from six.moves import range
 
 from . import version as simversion
@@ -293,3 +294,19 @@ def iter_array_split(part_index, N, M):
         end = start + length
 
     return range(start, end), end - start
+
+
+def jy2Tsr(f, bm=1.0, mK=False):
+    '''Return [K sr] / [Jy] vs. frequency (in Hz)
+        Arguments:
+            f = frequencies (Hz)
+            bm = Reference solid angle in steradians (Defaults to 1)
+            mK = Return in mK sr instead of K sr
+    '''
+    c_cmps = c.to('cm/s').value  # cm/s
+    k_boltz = 1.380658e-16   # erg/K
+    lam = c_cmps / f  # cm
+    fac = 1.0
+    if mK:
+        fac = 1e3
+    return 1e-23 * lam**2 / (2 * k_boltz * bm) * fac
