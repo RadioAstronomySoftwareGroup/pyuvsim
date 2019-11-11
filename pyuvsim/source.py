bugfix: ensuring that the correct angle units are used in array to/from skymodel

pyuvsim/source.py
@@ -327,11 +327,10 @@ class SkyModel(object):
                              'value was: {al}'.format(al=telescope_location))
 
         Ionly_mask = np.sum(self.stokes[1:, :, :], axis=0) == 0.0
-        NstokesI = np.sum(Ionly_mask)   # Number of unpolarized sources
+        NstokesI = np.sum(Ionly_mask[0,:])   # Number of unpolarized sources
 
         # For unpolarized sources, there's no need to rotate the coherency matrix.
         coherency_local = self.coherency_radec.copy()
-
         if NstokesI < self.Ncomponents:
             # If there are any polarized sources, do rotation.
             rotation_matrix = self._calc_coherency_rotation(telescope_location)
@@ -466,10 +465,9 @@ def healpix_to_sky(hpmap, indices, freqs):
     """
     Nside = astropy_healpix.npix_to_nside(hpmap.shape[-1])
     ra, dec = astropy_healpix.healpix_to_lonlat(indices, Nside)
-    freq = Quantity(freqs, 'hertz')
-    stokes = np.zeros((4, len(freq), len(indices)))
-    stokes[0] = (hpmap.T / simutils.jy2Tsr(freq,
-                                           bm=astropy_healpix.nside_to_pixel_area(Nside), mK=False)
+    stokes = np.zeros((4, len(freqs), len(indices)))
+    stokes[0] = (hpmap.T / simutils.jy2Tsr(freqs,
+                                           bm=astropy_healpix.nside_to_pixel_area(Nside).value, mK=False)
                  ).T
-    sky = SkyModel(indices.astype('str'), ra, dec, stokes, freq_array=freq, Nfreqs=len(freq))
+    sky = SkyModel(indices.astype('str'), ra, dec, stokes, freq_array=freqs, Nfreqs=len(freqs))
     return sky