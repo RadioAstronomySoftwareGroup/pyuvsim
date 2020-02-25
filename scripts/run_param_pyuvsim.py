b69c4fbe7d5238326f75a79fc07374a2131b0947
aelanman <adam_lanman@brown.edu>
2020-02-25 14:37:54 -0500

    update wrappers

--- scripts/run_param_pyuvsim.py
+++ scripts/run_param_pyuvsim.py
@@ -12,7 +12,8 @@ parser = argparse.ArgumentParser(
     description="A command-line script to execute a pyuvsim simulation from a parameter file."
 )
 parser.add_argument('paramsfile', type=str, help='Parameter yaml file.', default=None)
-parser.add_argument('--profile', type=str, help='Time profiling output file', default=None)
+parser.add_argument('--profile', type=str, help='Time profiling output file name.')
+parser.add_argument('--raw_profile', help='Also save pickled LineStats data for line profiling.', action='store_true')
 
 args = parser.parse_args()
 
@@ -20,7 +21,7 @@ if args.paramsfile is None:
     raise ValueError("Parameter file required")
 
 if args.profile is not None:
-    pyuvsim.profiling.set_profiler(outfile_name=args.profile)
+    pyuvsim.profiling.set_profiler(outfile_prefix=args.profile, dump_raw=args.raw_profile)
 
 if not os.path.isdir(os.path.dirname(args.paramsfile)):
     args.paramsfile = os.path.join('.', args.paramsfile)
