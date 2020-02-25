029ca7a34bda5fe77dca7418e9652aa89b4c98fb
aelanman <adam_lanman@brown.edu>
2020-02-25 14:36:33 -0500

    start adding benchmarking scripts

---
+++ benchmarking/analyze_runtimes.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+import sys
+from itertools import combinations
+import line_profiler as lp
+
+
+# Estimate scalings on different axes from profiling data.
+
+
+# NB -- This script cannot tell the difference between lines that are
+#          hit once and axes with length 1.
+#       It is only useful for jobs where the number of MPI processes is smaller
+#       than each simulation axis.
+
+
+# Only include functions that are called in loops.
+funcs_to_check = ['interp', 'get_beam_jones', 'apply_beam', 'make_visibility',
+                  'uvdata_to_task_iter', 'update_positions', 'coherency_calc']
+
+def _func_times(timings, Nlist, dt=1e-6):
+    outarr = np.zeros(len(Nlist))
+    for key, values in timings.items():
+        if key[2] not in funcs_to_check:
+            continue
+        for lnum, nhits, time in values:
+            print(key, lnum, nhits, nhits in Nlist)
+            if nhits in Nlist:
+                ind = Nlist.index(nhits)
+                outarr[ind] += dt * time  / nhits    # Time per hit.
+    return outarr
+
+profname = 'profdata/time_profile.lprof'
+axesname = 'profdata/time_profile_axes.npz'
+
+lstat = lp.load_stats(profname)
+axes_npz = np.load(axesname)
+axes = {k : axes_npz[k][0] for k in axes_npz.keys()}
+
+# Set up combinations of axes.
+Naxes = len(axes)
+
+combos = [
+    ('Ntimes',),
+    ('Ntimes', 'Nfreqs'),
+    ('Ntimes', 'Nfreqs', 'Nbls')
+]
+
+if not axes['Nsrcs_loc'] == 1:
+    combos.append(('Ntimes', 'Nfreqs', 'Nbls', 'Nsrcs_loc'))
+
+#combs = []
+#for ii in range(1, Naxes+1):
+#    combs.extend(list(combinations(axes.keys(), ii)))
+
+Nlist = []
+for comb in combos:
+    length = 1
+    print(comb)
+    for key in comb:
+        length *= axes[key]
+    Nlist.append(length)
+
+results = _func_times(lstat.timings, Nlist, dt=lstat.unit)
+
+import IPython; IPython.embed()
