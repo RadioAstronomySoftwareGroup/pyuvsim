e0431ea27a09e4ab733d9e0fb80d9a2c2cfc568f
Adam Lanman <adam_lanman@brown.edu>
2020-03-19 22:51:36 -0400

    updates to run_ref_sims.sh

--- reference_simulations/run_ref_sims.sh
+++ reference_simulations/run_ref_sims.sh
@@ -1,32 +1,61 @@
 #!/bin/bash
 
-
 # Run reference simulations and save slurmIDs
 
 obsparam_files=( "$@" )
 nfiles="$#"
 
-echo ${obsparam_files[@]}
-echo $nfiles
-
-dirname=$(date +%Y-%m-%d)   # Name directory with today's date.
-mkdir -p $dirname"/profiling"
-mkdir -p $dirname"/slurm_out"
-mkdir -p $dirname"/slurm_out"
-
-prevdir=$(pwd -P)
-
-ln -sf $prevdir/telescope_config $dirname/telescope_config
-ln -sf $prevdir/catalog_files $dirname/catalog_files
+v1files=()
+v2files=()
 
 for fn in "${obsparam_files[@]}"
 do
-        fn=$(basename $fn)
-        echo $fn
-        cp $fn $dirname/$fn
+        fdir=$(dirname $fn)
+        fnme=$(basename $fn)
+        if [[ $fdir = "first_reference_simulations" ]]
+        then
+                v1files+=($fnme)
+        fi
+        if [[ $fdir = "second_reference_simulations" ]]
+        then
+                v2files+=($fnme)
+        fi
 done
+prevdir=$(pwd -P)
+dirname=$(date +%Y-%m-%d)       # Name directory with today's date.
 
+if [[ ${#v1files[@]} -gt 0 ]]
+then
+    fulldir=$dirname"_v1"
+    echo $fulldir
+    sourcedir=$prevdir"/first_reference_simulations"
+    mkdir -p $fulldir"/profiling"
+    mkdir -p $fulldir"/slurm_out"
+    ln -sf $sourcedir/telescope_config $fulldir/telescope_config
+    ln -sf $sourcedir/catalog_files $fulldir/catalog_files
+    for fn in "${v1files[@]}"
+    do
+            cp 'first_reference_simulations/'$fn $fulldir"/"$(basename $fn)
+    done
+    cd $fulldir
+    sbatch -o slurm_out/pyuvsim-%A_%a.out --array=0-$(( ${#v1files[@]} - 1  )) $prevdir/jobscript.sh ${v1files[@]}
+    cd $prevdir
+fi
 
-cd $dirname
-
-sbatch -o slurm_out/pyuvsim-%A_%a.out --array=0-$(( $nfiles - 1  )) $prevdir/ref_sim_job.sh ${obsparam_files[@]}
+if [[ ${#v2files[@]} -gt 0 ]]
+then
+    fulldir=$dirname"_v2"
+    echo $fulldir
+    sourcedir=$prevdir"/second_reference_simulations"
+    mkdir -p $fulldir"/profiling"
+    mkdir -p $fulldir"/slurm_out"
+    ln -sf $sourcedir/telescope_config $fulldir/telescope_config
+    ln -sf $sourcedir/catalog_files $fulldir/catalog_files
+    for fn in "${v1files[@]}"
+    do
+            cp 'second_reference_simulations/'$fn $fulldir"/"$(basename $fn)
+    done
+    cd $fulldir
+    sbatch -o slurm_out/pyuvsim-%A_%a.out --array=0-$(( ${#v2files[@]} - 1  )) $prevdir/jobscript.sh ${v2files[@]}
+    cd $prevdir
+fi
