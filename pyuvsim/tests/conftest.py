75f5aa0f70ae5af0ebb0f1b0ed293e8879a85916
Adam Lanman <adam_lanman@brown.edu>
2020-07-14 13:26:08 -0400

    fix argparse

--- pyuvsim/tests/conftest.py
+++ pyuvsim/tests/conftest.py
@@ -56,7 +56,7 @@ def pytest_runtest_setup(item):
     if 'parallel' in item.keywords:
         if mpi is None:
             pytest.skip("Need mpi4py to run parallelized test.")
-        elif item.config.getvalue('nompi'):
+        elif item.config.getoption('nompi', False):
             pytest.skip("Skipping parallelized tests with --nompi option.")
 
 
