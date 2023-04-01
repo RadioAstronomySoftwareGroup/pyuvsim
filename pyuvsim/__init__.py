72b0b79f4da9ce4276f89bf820a26352f88fb5a7
Bryna Hazelton <bryna.hazelton@gmail.com>
2023-03-31 18:08:04 -0700

    use importlib rather than pkg_resources

--- pyuvsim/__init__.py
+++ pyuvsim/__init__.py
@@ -2,14 +2,14 @@
 # Copyright (c) 2018 Radio Astronomy Software Group
 # Licensed under the 3-clause BSD License
 """Define namespace."""
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 
-from pkg_resources import DistributionNotFound, get_distribution
 from setuptools_scm import get_version
 
 from .branch_scheme import branch_scheme
 
-try:  # pragma: nocover
+try:
     # get accurate version for developer installs
     version_str = get_version(Path(__file__).parent.parent, local_scheme=branch_scheme)
 
@@ -18,8 +18,8 @@ try:  # pragma: nocover
 except (LookupError, ImportError):
     try:
         # Set the version automatically from the package details.
-        __version__ = get_distribution(__name__).version
-    except DistributionNotFound:  # pragma: nocover
+        __version__ = version("pyuvsim")
+    except PackageNotFoundError:  # pragma: nocover
         # package is not installed
         pass
 
