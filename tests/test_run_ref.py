4e937305a682eebe1b0bfe4224936fbab355c812
Mitchell Burdorf <burdorfmitchell@gmail.com>
2024-10-23 21:47:56 -0400

    formatting

--- tests/test_run_ref.py
+++ tests/test_run_ref.py
@@ -1,50 +1,52 @@
 # Copyright (c) 2020 Radio Astronomy Software Group
 # Licensed under the 3-clause BSD License
 
+import importlib
 import os
-#import warnings
-
 
 import pytest
 from pyuvdata import UVData
 
-import pyuvsim
 from pyuvsim.data import DATA_PATH as SIM_DATA_PATH
+from pyuvsim.mpi import rank
+from pyuvsim.uvsim import run_uvsim
 
-import importlib
 hasbench = importlib.util.find_spec("pytest_benchmark") is not None
 
 pytest.importorskip("mpi4py")  # noqa
 
 # TODO: get running for current 8 sims (add necessary data files)
-# AND DO THE FIX FOR THE GOOGLE DRIVE (UPLOAD THE FILES AND 
-# GET THEM TO DOWNLOAD (I GUESS TEMP JUST MAKE ANOTHER 
+# AND DO THE FIX FOR THE GOOGLE DRIVE (UPLOAD THE FILES AND
+# GET THEM TO DOWNLOAD (I GUESS TEMP JUST MAKE ANOTHER
 # DICT OR FILE OR DATA STRUCTURE THAT CAN GRAB THE FILES)
 
 # TODO: check syntax preference for global lists!
 ci_ref_sims = [
-               "1.1_uniform",
-               "1.1_hera",
-               "1.1_gauss",
-               "1.2_uniform",
-               "1.2_hera",
-               "1.3_uniform",
-               "1.3_gauss"
-              ]
+    "1.1_uniform",
+    "1.1_hera",
+    "1.1_gauss",
+    "1.2_uniform",
+    "1.2_hera",
+    "1.3_uniform",
+    "1.3_gauss",
+]
 
+
+# TODO: hardset it using dictionary or something
 # TODO: swap to something more permanent!
 @pytest.fixture
 def download_sims():
     import requests
-    target_dir = "results_data" 
+
+    target_dir = "results_data"
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
     urlbase = "https://drive.google.com/uc?export=download"
 
-    dat = []
-
+    # TODO: upload all other files and hardcode
+    #       them via a dictionary with ci_ref_sims for keys
     fid = "1V4RmmUGrx5iH-Zyuj45p1ag3vmMuUI2A"
     fname = "ref_1.1_uniform.uvh5"
     # fileid name type size size_unit date time
@@ -70,22 +72,21 @@ def goto_tempdir(tmpdir):
 
 
 # TODO: add all 8 yaml files
-@pytest.mark.parametrize("paramfile", ci_ref_sims) 
+@pytest.mark.parametrize("paramfile", ci_ref_sims)
 @pytest.mark.skipif(not hasbench, reason="benchmark utility not installed")
 def test_run_11_uniform(benchmark, goto_tempdir, download_sims, paramfile):
     # download reference sim
-    #download_sims(goto_tempdir)
+    # download_sims(goto_tempdir)
 
     # construct filename and filepath of downloaded file
     uvh5_filename = "ref_" + paramfile + ".uvh5"
     uvh5_filepath = os.path.join(goto_tempdir, "results_data", uvh5_filename)
 
-    # instantiate UVData object and read in the downloaded uvh5 
+    # instantiate UVData object and read in the downloaded uvh5
     # file as the point of historical comparison
-    uv_ref = UVData()
-    uv_ref.read_uvh5(filepath)
+    uv_ref = UVData.from_file(uvh5_filepath)
 
-    # construct filename and filepath of yaml file used 
+    # construct filename and filepath of yaml file used
     # to run simulation
     yaml_filename = "obsparam_ref_" + paramfile + ".yaml"
     yaml_filepath = os.path.join(SIM_DATA_PATH, "test_config", yaml_filename)
@@ -94,25 +95,21 @@ def test_run_11_uniform(benchmark, goto_tempdir, download_sims, paramfile):
     # runs around 10 times to get benchmark data
     # outdir is given by the yaml file but should be current working directory
     # for all the reference simulations
-    # TODO: think more about filepath specification, also there was a warning I can get rid of probably
+    # TODO: think more about filepath specification, also there was a warning
     # TODO: check if somehow making like 10 copies of output accidentally (whoops)
-    benchmark(
-        pyuvsim.uvsim.run_uvsim,
-        yaml_filepath
-    )
+    benchmark(run_uvsim, yaml_filepath)
 
     # loading the file and comparing is only done on rank 0.
-    if pyuvsim.mpi.rank != 0:
+    if rank != 0:
         return
 
     # instantiate new UVData object from the benchmark run_uvsim output
     # as current point of comparison
-    path = goto_tempdir
-    ofilepath = os.path.join(path, uvh5_filename)
-    uv_new = UVData.from_file(ofilepath)
+    new_uvh5_filepath = os.path.join(goto_tempdir, uvh5_filename)
+    uv_new = UVData.from_file(new_uvh5_filepath)
 
-    # reset part(s) that should deviate 
-    # TODO: maybe assert that deviation occurred 
+    # fix part(s) that should deviate
+    # TODO: maybe assert that deviation occurred
     uv_new.history = uv_ref.history
 
     # perform equality check between historical and current reference
