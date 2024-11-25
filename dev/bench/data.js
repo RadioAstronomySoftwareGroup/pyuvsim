d14912b79d93e5ced4696db0190d8abc7015db49
github-action-benchmark <github@users.noreply.github.com>
2024-11-25 14:52:04 +0000

    add Benchmark (pytest) benchmark result for e48d72ac6c1a63ac230c8b7c4d2af0ee578eda9f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732545642247,
+  "lastUpdate": 1732546324894,
   "repoUrl": "https://github.com/RadioAstronomySoftwareGroup/pyuvsim",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.022654311787191635",
             "extra": "mean: 1.5426127506000058 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "burdorfmitchell@gmail.com",
+            "name": "Mitchell Burdorf",
+            "username": "burdorfmitchell"
+          },
+          "committer": {
+            "email": "burdorfmitchell@gmail.com",
+            "name": "Mitchell Burdorf",
+            "username": "burdorfmitchell"
+          },
+          "distinct": true,
+          "id": "e48d72ac6c1a63ac230c8b7c4d2af0ee578eda9f",
+          "message": "dummy change",
+          "timestamp": "2024-11-25T09:46:52-05:00",
+          "tree_id": "f82c9b548599caa9096e7d602c569e8263bb54fa",
+          "url": "https://github.com/RadioAstronomySoftwareGroup/pyuvsim/commit/e48d72ac6c1a63ac230c8b7c4d2af0ee578eda9f"
+        },
+        "date": 1732546324590,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_run_ref.py::test_run_sim[1.1_gauss]",
+            "value": 0.6710736539695343,
+            "unit": "iter/sec",
+            "range": "stddev: 0.025067797231711753",
+            "extra": "mean: 1.4901493958000003 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_run_ref.py::test_run_sim[1.1_uniform]",
+            "value": 0.6583124335692239,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03028631947542372",
+            "extra": "mean: 1.5190355657999988 sec\nrounds: 5"
           }
         ]
       }
