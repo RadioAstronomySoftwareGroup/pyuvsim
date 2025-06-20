586187161135d488b79d8c1d3cf2c8148d8aae2f
Mitchell Burdorf <burdorfmitchell@gmail.com>
2025-06-20 02:52:24 -0400

    implemented astropy approach that should be robust for downloading data files

--- reference_simulations/download_data_files.py
+++ reference_simulations/download_data_files.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2022 Radio Astronomy Software Group
 # Licensed under the 3-clause BSD License
 """
-An extensible approach to large file downloading with variable methods -- beams and catalogs.
+An approach to large file downloading using the astropy cache to store beams and catalogs.
 
 Takes a command line argument of the file to download.
 
@@ -11,67 +11,108 @@ Current options:
     - "mwa": mwa uvbeam file
     - "healpix": gsm 2016 nside 128 healpix map saved as skyh5
 
-gleam requires pyradiosky and astroquery, mwa and healpix require pooch.
+All files require astropy. Gleam additionally requires pyradiosky and astroquery.
 
-downloads files to pooch default file download location: "~/.cache/pooch/"
+Downloads files to astropy default cache location for pyuvsim e.g. "get_cache_dir(pyuvsim)"
 
-approach inspired by: https://stackoverflow.com/questions/67790907/how-to-call-a-function-stored-as-variable-with-arguments-python-3
+The intention is to have all files downloaded under the astropy paradigm, and thus findable
+in cache throug the same paradigm with methods get_cached_urls and download_file. This allows
+us to list only the file url corresponding to the astropy cached file in either yaml instead
+of having to worry about relative or absolute path variables.
+
 """
 
 import argparse
+import os
 
 try:
-    from pooch import os_cache, retrieve
+    from astropy.config import get_cache_dir
+    from astropy.utils.data import download_file, get_cached_urls, import_file_to_cache
     from pyradiosky.utils import download_gleam
 except ImportError as e:
     raise ImportError(
-        "pooch, pyradiosky, astroquery are required for full script functionality"
+        "astropy, pyradiosky, astroquery are required for full script functionality"
     ) from e
 
 
-# TODO: need to figure out if there is a more proper "pooch" approach to downloading and storing
-#       the files even when pooch isn't used to download the file
-
-# TODO: fully replace this file with the proper method of downloading pooch files
-
-# TODO: maybe change to pyuvsim_reference_simulations?
-# TODO: need to figure out how to properly configure this target via yaml files
-file_cache = os_cache("pyuvsim")
+# get the cache directory for pyuvsim as proscribed by astropy
+cache_dir = get_cache_dir("pyuvsim")
 
 
-# dictionary of keywords for downloadable files
-# each downloadable file has a dictionary containing:
-# - the function used to download the file
-# - the args to pass to the function
-# - the kwargs to pass to the function
+# dictionary of keywords for downloadable files with url values (gleam we don't have a url)
 file_download_dict = {
     # mwa uvbeam
-    "mwa": {
-        "function": retrieve,
-        "args": [],
-        "kwargs": {
-            "url": "http://ws.mwatelescope.org/static/mwa_full_embedded_element_pattern.h5",
-            "fname": "mwa_full_embedded_element_pattern.h5",
-            "known_hash": "a7649c6e03b8128a1de4614c2f363af5fa44f3890ae27bf893d56ca337bc48ee",
-            # "progressbar": True,
-            "path": file_cache,
-        },
-    },
+    "mwa": "http://ws.mwatelescope.org/static/mwa_full_embedded_element_pattern.h5",
     # gsm 2016 nside 128 healpix map
-    "healpix": {
-        "function": retrieve,
-        "args": [],
-        "kwargs": {
-            "url": "https://repository.library.brown.edu/storage/bdr:eafzyycj/content/",
-            "fname": "gsm16_nside128_100mhz.skyh5",
-            "known_hash": "cedbbe5180830e69cefe72c3a1642a8205dc2d595e6bc0a35c08e5948b12290d",
-            # "progressbar": True,
-            "path": file_cache,
-        },
-    },
+    "healpix": "https://repository.library.brown.edu/storage/bdr:eafzyycj/content/",
     # gleam sky catalog as VOTable
-    "gleam": {"function": download_gleam, "args": [], "kwargs": {"path": file_cache}},
+    "gleam": "",
 }
+
+
+def download_gleam_vot(url="file://gleam.vot"):
+    """
+    Download gleam.vot to pyuvsim cache specified by astropy.
+
+    Runs import_file_to_cache to format the installed file to be recognizable through astropy.
+    Includes adding a fake url with which to specify the file. The fake download url acts as
+    the key to load the file in astropy -- allowing filename specification in yaml files
+    without any path details as astropy handles searching the cache with get_cached_urls
+    and download_file.
+
+    Parameters
+    ----------
+    url: str
+        A fake url for the gleam.vot file.
+
+    """
+    # check if url already cached
+    if url in get_cached_urls("pyuvsim"):
+        print(
+            f"astropy cached url for {url} already exists! If you want to redownload,"
+            " please clear the cached url."
+        )
+        return
+
+    print("gleam.vot download can take a while")
+    # downloads gleam.vot using astroquery
+    download_gleam(path=cache_dir, filename="gleam.vot")
+
+    path_to_file = os.path.join(cache_dir, "gleam.vot")
+
+    # converts downloaded gleam.vot to format recognizable for astropy with fake url key
+    if os.path.exists(path_to_file):
+        print("running import_file_to_cache to convert to astropy downloaded format")
+        import_file_to_cache(url, path_to_file, remove_original=True, pkgname="pyuvsim")
+    else:
+        print(f"gleam.vot not downloaded to expected location: {path_to_file}")
+
+
+def download_file_using_astropy(url):
+    """
+    Download file at url to pyuvsim cache specified by astropy using download_file.
+
+    Parameters
+    ----------
+    url: str
+        The url of the file to be downloaded.
+
+    The download url acts as the key to load the file in astropy -- allowing filename
+    specification in yaml files without any path details as astropy handles searching
+    the cache with get_cached_urls and download_file.
+
+    """
+    # check if url already cached
+    if url in get_cached_urls("pyuvsim"):
+        print(
+            f"astropy cached url for {url} already exists! If you want to redownload,"
+            " please clear the cached url."
+        )
+        return
+
+    # astropy file download method which defaults to cached file and hashes the file
+    # should have a default timeout of 10 seconds
+    download_file(url, cache=True, pkgname="pyuvsim")
 
 
 def main():
@@ -104,51 +145,16 @@ def main():
     # for each file keyword, check that it exists in the dictionary, then try to download
     for file in files:
         if file in file_download_dict:
-            # get download instructions for file
-            download_instructions = file_download_dict[file]
-            # call method which construct download call from instruction
-            download_from_instructions(file, download_instructions)
+            # call download_gleam_vot if gleam, otherwise download from url
+            if file == "gleam":
+                download_gleam_vot()
+            else:
+                download_file_using_astropy(file_download_dict[file])
         else:
             print(
                 f'file "{file}" not found! Check the available keys in the file_download_dict'
                 "or the listed current options at the top of the file."
             )
-
-
-def download_from_instructions(filename, instruction):
-    """
-    Take as input the filename and a dictionary containing three keys.
-
-    Uses the dictionary to construct a download method to download the
-    file, and calls the method.
-
-    Parameters
-    ----------
-    filename:  str
-        The filename only used for print statement logic.
-    instruction: dict
-        A dictionary containing the:
-        - function: the method to call to download the file(s)
-        - args: a list of arguments to pass to the method in order
-        - kwargs: a list of keyword arguments to pass to the method
-          then calls the method using the args and kwargs
-
-    """
-    # parse download instructions
-    download_method = instruction.get("function")
-    args = instruction.get("args", [])
-    kwargs = instruction.get("kwargs", {})
-
-    if "path" in kwargs:
-        print(f"downloading {filename} to {kwargs['path']}")
-    else:
-        print(f"download path for {filename} not specified in dictionary")
-
-    if filename == "gleam":
-        print("gleam filename download can take a while")
-
-    # call the method with args and kwargs to download the file
-    download_method(*args, **kwargs)
 
 
 if __name__ == "__main__":
