3663a297b9c0200dfb221fea2cd9fd5973eb001d
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-07-08 11:19:11 -0700

    move everything out of scripts and into a new cli module
    update the pyproject.toml accordingly

--- scripts/test_cover_noinstall.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
-
-cd pyuvsim/tests
-python -m pytest --cov=pyuvsim --cov-config=../../.coveragerc\
-       --cov-report term --cov-report html:cover \
-       "$@"
