65cd91365d28850ac2db42e654eb362ff9af25f3
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-08-05 17:21:50 -0700

    add tests for uvdata_to_telescope_config

--- src/pyuvsim/cli.py
+++ src/pyuvsim/cli.py
@@ -3,7 +3,6 @@
 """Command line scripts."""
 
 import argparse
-import os
 import time as pytime
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -18,7 +17,6 @@ from pyradiosky import SkyModel
 from pyuvdata import UVBeam, UVData
 
 from pyuvsim import profiling, simsetup, telescope, utils, uvsim
-from pyuvsim.data import DATA_PATH as SIM_DATA_PATH
 from pyuvsim.simsetup import _parse_layout_csv as parse_csv
 
 
@@ -165,23 +163,21 @@ def uvdata_to_telescope_config(argv=None):
     )
 
     parser.add_argument("file_in", metavar="<FILE>", type=str, nargs="+")
+    parser.add_argument("-b", "--beamfile", type=str, default=None)
     parser.add_argument("-l", "--layout_csv_name", default=None)
     parser.add_argument("-t", "--telescope_config_name", default=None)
-    parser.add_argument(
-        "-b",
-        "--beam_filepath",
-        type=str,
-        default=os.path.join(SIM_DATA_PATH, "HERA_NicCST.uvbeam"),
-    )
 
     args = parser.parse_args(argv)
+
+    if args.beamfile is None:
+        raise ValueError("beamfile must be passed.")
 
     uvd = UVData()
     uvd.read(args.file_in[0])
 
     simsetup.uvdata_to_telescope_config(
         uvd,
-        args.beam_filepath,
+        args.beamfile,
         layout_csv_name=args.layout_csv_name,
         telescope_config_name=args.telescope_config_name,
         return_names=False,
